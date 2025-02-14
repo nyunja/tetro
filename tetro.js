b9f12790082b69ec8f9698335c175562c312133a
nyunja <poptiff@gmail.com>
2025-02-14 17:05:07 +0300

    feat: implement move right, left and rotate

--- tetro.js
+++ tetro.js
@@ -1,9 +1,128 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('.grid');
-    let squares = Array.from(document.querySelectorAll('.grid div'));
-    const width = 10;
-    const score = document.querySelector('#score');
-    const startBtn = document.querySelector('#start-button');
-    console.log(squares);
+document.addEventListener("DOMContentLoaded", () => {
+    const grid = document.querySelector(".grid");
+    const cells = Array.from(document.querySelectorAll(".grid div"));
+    const startBtn = document.querySelector("#start-button");
 
+    const width = 10;
+    const lTetrominoes = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width +1, width + 2, width*2 + 2],
+        [1, width+1, width *2+ 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2],
+    ];
+
+    const zTetrpminoes = [
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+    ];
+
+    const tTetrominoes = [
+        [1, width, width+1, width+2],
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1],
+    ];
+
+    const oTetrominoes = [
+        [0,1,width, width+1],
+        [0,1,width, width+1],
+        [0,1,width, width+1],
+        [0,1,width, width+1],
+    ];
+
+    const iTetrominoes = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+    ];
+
+    const tetrominoes = [lTetrominoes, zTetrpminoes, tTetrominoes, oTetrominoes, iTetrominoes];
+
+    let currentPosition = 4;
+    let currentRotation = 0;
+
+    let random = Math.floor(Math.random()*tetrominoes.length);
+    let currentTetromino =tetrominoes[random][currentRotation];
+
+    function draw() {
+        currentTetromino.forEach(index => {
+            cells[currentPosition + index].classList.add("tetromino");
+        });
+    }
+
+    function undraw() {
+        currentTetromino.forEach(index => {
+            cells[currentPosition + index].classList.remove("tetromino");
+        });
+    }
+
+    let timerId = setInterval(moveDown, 1000);
+
+    function control(e) {
+        if (e.keyCode === 37) {
+            moveLeft();
+        } else if (e.keyCode === 39) {
+            moveRight();
+        } else if (e.keyCode === 38) {
+            updateRotation();
+        }
+    }
+    function updateRotation() {
+        undraw();
+        currentRotation++;
+        if (currentRotation === 4) currentRotation = 0;
+        currentTetromino = tetrominoes[random][currentRotation];
+        draw();
+    }
+
+    document.addEventListener("keyup", control);
+
+    function moveDown() {
+        undraw();
+        currentPosition += width;
+        draw();
+        freeze();
+    }
+
+    function freeze() {
+        if (currentTetromino.some(index => cells[currentPosition + index + width].classList.contains("taken"))) {
+            currentTetromino.forEach(index => cells[currentPosition + index].classList.add("taken"));
+            random = Math.floor(Math.random()*tetrominoes.length);
+            currentTetromino = tetrominoes[random][currentRotation];
+            currentPosition = 4;
+            draw();
+        };
+    }
+
+    function moveLeft() {
+        undraw();
+        const isAtLeftEdge = currentTetromino.some(index => (currentPosition + index) % width === 0);
+    
+        if (!isAtLeftEdge) {
+            currentPosition -= 1;
+        }
+
+        if (currentTetromino.some(index => cells[currentPosition + index + width].classList.contains("taken"))) {
+            currentPosition += 1;
+        }
+        draw();
+    }
+
+    function moveRight() {
+        undraw();
+        const isAtRightEdge = currentTetromino.some(index => (currentPosition + index) % width === 9);
+    
+        if (!isAtRightEdge) {
+            currentPosition += 1;
+        }
+
+        if (currentTetromino.some(index => cells[currentPosition + index + width].classList.contains("taken"))) {
+            currentPosition -= 1;
+        }
+        draw();
+    }
 });
+
